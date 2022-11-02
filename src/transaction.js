d84cce290517d4dc729d4fce9fd86b0330bd01c8
Salva <salvinha1702@gmail.com>
2022-11-02 19:28:44 -0300

    optimizando logica y vista

--- src/transaction.js
+++ src/transaction.js
@@ -10,52 +10,39 @@ fetch('https://635fe51f3e8f65f283be4b05.mockapi.io/users/' + user.id + '/transac
 
         datos = ""
         u('#picture').attr("src", user.avatar)
-        const header = document.getElementById('header')
         const nameuser = document.getElementById('nameuser')
         nameuser.innerHTML = user.name
-        const listadedatos = document.getElementById('listadatos')
         let i = 0
+        
+        
+        contenidodatos=u('fieldset#content li')
+        
         data.forEach(item => {
             const description = item.description.split(' ')
             const array = description.toString().slice(0, 30)
             const string = array.replace(/,/g, ' ')
-            i++
+            const date = new Date(item.createdAt).toDateString()
+            
+            
+            let clon = contenidodatos.clone()
+            
+            clon.find('.trxcreatedAt').text(date)
+            clon.find('.description').text(item.description)
+            clon.find('.titulo').text(string)
+            //clon.find('.amount').text(item.amount)
+            u('fieldset#content').append(clon)
+            
             if (item.description.includes("deposit")) {
-                datos += `
-                <br>
-        <li id="block-trans" class="list-group-item d-flex justify-content-between align-items-start shadow-sm p-3 mb-5 bg-body rounded" type="button" data-bs-toggle="collapse" data-bs-target="#${description[i]}" aria-expanded="false" aria-controls="${description[i]}">
-                <div  class="ms-5 me-auto">
-                <div class="fw-bold">${item.createdAt}</div>
-                <p type="button" data-bs-toggle="collapse" data-bs-target="#${description[i]}" aria-expanded="false" aria-controls="${description[i]}">${string}</p><div class="collapse" id="${description[i]}"><div class="card card-body">${item.description}</div></div>
-                </div>
-                <span id="amount" class="badge bg-success rounded-pill"> + ${item.amount}</span>
-        </li>
-        `
-            } if (item.description.includes("pay")) {
-                datos += `
-                <br>
-                <li id="block-trans" class="list-group-item d-flex justify-content-between align-items-start shadow-sm p-3 mb-5 bg-body rounded" type="button" data-bs-toggle="collapse" data-bs-target="#${description[i]}" aria-expanded="false" aria-controls="${description[i]}">
-                        <div  class="ms-5 me-auto">
-                        <div class="fw-bold">${item.createdAt}</div>
-                        <p type="button" data-bs-toggle="collapse" data-bs-target="#${description[i]}" aria-expanded="false" aria-controls="${description[i]}">${string}</p><div class="collapse" id="${description[i]}"><div class="card card-body">${item.description}</div></div>
-                        </div>
-                        <span id="amount" class="badge bg-danger rounded-pill"> + ${item.amount}</span>
-                </li>
-                `
+                clon.find('.amount').text('+ '+item.amount).addClass('fw-bold text-success fs-5')
+                
+            } 
+            if (item.description.includes("pay")) {
+                clon.find('.amount').text('- '+item.amount).addClass('fw-bold text-danger fs-5')
             } if (item.description.includes("invoice")) {
-                datos += `
-                <br>
-                <li id="block-trans" class="list-group-item d-flex justify-content-between align-items-start shadow-sm p-3 mb-5 bg-body rounded" type="button" data-bs-toggle="collapse" data-bs-target="#${description[i]}" aria-expanded="false" aria-controls="${description[i]}">
-                        <div  class="ms-5 me-auto">
-                        <div class="fw-bold">${item.createdAt}</div>
-                        <p type="button" data-bs-toggle="collapse" data-bs-target="#${description[i]}" aria-expanded="false" aria-controls="${description[i]}">${string}</p><div class="collapse" id="${description[i]}"><div class="card card-body">${item.description}</div></div>
-                        </div>
-                        <span id="amount" class="badge bg-secondary rounded-pill"> + ${item.amount}</span>
-                </li>
-                `
+                clon.find('.amount').text(' '+item.amount).addClass('fw-bold text-secondary fs-5')
             }
         });
-        listadedatos.innerHTML = datos
+        //listadedatos.innerHTML = datos
 
     })
 
