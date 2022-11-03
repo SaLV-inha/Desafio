e8bbfab00e363ed152d1d0adb58757a1296627b2
Salva <salvinha1702@gmail.com>
2022-11-03 13:16:44 -0300

    Adición de grafica

--- src/transaction.js
+++ src/transaction.js
@@ -1,53 +1,67 @@
 if (!window.sessionStorage.getItem('user')) {
     window.location.href = './index2.html'
 }
-
 const user = JSON.parse(window.sessionStorage.getItem('user'))
 
 fetch('https://635fe51f3e8f65f283be4b05.mockapi.io/users/' + user.id + '/transactions')
 .then((data) => data.json())
 .then(data => {
-    
-    datos = ""
     u('#picture').attr("src", user.avatar)
     const nameuser = document.getElementById('nameuser')
-    nameuser.innerHTML = user.name
-    
-    
+    nameuser.innerHTML = `Hola,  ${user.name}`
     contenidodatos=u('fieldset#content li')
+
+    let datospie = []
+    let depositos = 0
+    let pagos = 0
+    let otros = 0
+
     data.forEach((item, i) => {
         const description = item.description.split(' ')
         const array = description.toString().slice(0, 30)
         const string = array.replace(/,/g, ' ')
         const date = new Date(item.createdAt).toDateString()
-        
         let clon = contenidodatos.clone()
-        
         clon.find('.trxcreatedAt').text(date)
         clon.find('.description').text(item.description).parent().data('t', 'd' + i)
         clon.find('.titulo').text(string)
         clon.data('bs-target', '[data-t="d' + i + '"]').data('bs-toggle', 'collapse')
-        //clon.append(u('<a>').data('bs-target', '.description').data('bs-toggle', 'collapse').text('asdasd'))
+        clon.removeClass('d-none')
         u('fieldset#content').append(clon)        
             if (item.description.includes("deposit")) {
-                clon.find('.amount').text('+'+item.amount).addClass('fw-bold text-success fs-5')
-                
-            } 
-            if (item.description.includes("pay")||item.description.includes("withdrawal")) {
-                clon.find('.amount').text('-'+item.amount).addClass('fw-bold text-danger fs-5')
+                clon.find('.amount').text(`+$`+item.amount).addClass('fw-bold text-success fs-5')
+                depositos += parseInt(item.amount)
+            } if (item.description.includes("pay")||item.description.includes("withdrawal")) {
+                clon.find('.amount').text('-$'+item.amount).addClass('fw-bold text-danger fs-5')
+                pagos += parseInt(item.amount)
             } if (item.description.includes("invoice")) {
-                clon.find('.amount').text(+item.amount).addClass('fw-bold text-secondary fs-5')
-            }
+                clon.find('.amount').text('$'+item.amount).addClass('fw-bold text-secondary fs-5')
+                otros += parseInt(item.amount)
+            }        
             
-    });
-        
-        const collapseElementList = document.querySelectorAll('.collapse')
-        const collapseList = [...collapseElementList].map(collapseEl => {
-            let c = new bootstrap.Collapse(collapseEl, {toggle: false})
-        })
+        });    
+        let ingresos = depositos + otros
+        let total = ingresos - pagos
+        u('#movimientos').text(`Ingresos: $${depositos} `)
+        u('#movimientos').append(`Egresos: $${pagos} `)
+        u('#movimientos').append(`TOTAL: $${total} `)
+        u('#saldo').text(`$${total}`)
+       
+        var data = [{
+            values: [ingresos, pagos],
+            labels: ['Depositos', 'Pagos o Retiros'],
+            type: 'pie'
+          }];
+          
+          var layout = {
+            height: 300,
+            width: 400
+          };
+          
+          Plotly.newPlot('myDiv', data, layout);
+          
+})    
 
-    })    
-    
 u('#btn-logout').on('click', function (e) {
     e.preventDefault();
     window.sessionStorage.removeItem("user")
